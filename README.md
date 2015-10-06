799ac842e74739f4b5d23be009d10944de9257f6
LaNsHoR <lanshor@gmail.com>
2015-10-06 13:32:52 +0200

    Release the first version

---
+++ README.md
@@ -0,0 +1,283 @@
+#BananaDB
+
+*A class for easy and secure access to MySQL from PHP.*
+
+Copyright (C) 2015 LaNsHoR lanshor@gmail.com
+
+This program is free software: you can redistribute it and/or modify it under the terms of the MIT License (see the code for more info).
+
+##Goals
+
+1. **SQL injection protection**. BananaDB protects you and makes SQL injection attacks 100% impossible.
+2. **Easy prepared statements**. With BananaDB, the use of prepared statements is automatic and trivial.
+3. **Ultra simple set up**. One file, one include, lot of fun.
+4. **SQL syntax**. Write your PHP code like SQL. Do more with less lines. Gains readability.
+5. **Exceptions ready**. If something goes wrong, BananaDB throws a exception with a descriptive message.
+
+## Set Up
+
+```php
+require_once("banana_db.php");
+$bdb = new BananaDB("localhost", "my_user", "my_password", "my_database");
+```
+
+That's all folks!
+
+If you don't like the bananas, you can also use the class alias BDB.
+
+```php
+$bdb = new BDB("localhost", "my_user", "my_password", "my_database");
+```
+
+More pro. Less funny.
+
+## Select
+
+### Basic select
+
+```php
+$all_my_fruits = $bdb->select("*")->from("fruits")->exec();
+```
+
+or
+
+```php
+$bdb->select("*")->from("fruits");
+$all_my_fruits = $bdb->exec();
+```
+
+Both are the same. Easy, right?
+
+### Reading a row
+
+```php
+$first_fruit_row = $all_my_fruits[0]; //The first line of the table
+$second_fruit_row = $all_my_fruits[1]; //The second line of the table
+//... etc
+```
+
+As you can see, it's so easy. Surely you can imagine how to read a field. You probably don't need to read the next section :)
+
+### Reading a field from a row
+
+```php
+$id_of_first_fruit = $first_fruit_row["id_fruit"];
+```
+or
+```php
+$id_of_frist_fruit = $first_fruit_row[0]; //id_fruit is the first column of the table, them index = 0
+```
+
+You can read a field through the name or index. Both are the same.
+
+### Take what you want
+
+You can execute your query with:
+
+- **exec**: returns a two dimensional array with the result table.
+- **exec_one_row**: returns an associative array with the first row of the result.
+- **exec_one_field**: returns the first value, of the first row, of the result.
+
+```php
+$fruit_name = $bdb->selec("name")->from("fruits")->where("id_fruit = 5")->and("active = 1")->exec_one_field();
+```
+
+Get values in only one line. As it always should be.
+
+### Using variables
+
+In order to use variables in your query, you need to extract them from the strings.
+
+```php
+$id_fruit = 5;
+$color = "red";
+$fruit_name = $bdb->selec("name")->from("fruits")->where("id_fruit =", $id_fruit)->and("active = 1")->or("color =", $color)->exec_one_field();
+```
+
+This protects you from SQL injection attacks and enable the possibility of use prepared statements (see below).
+
+**++/!\++ WARNING 1**: Variables inside strings are not checked and makes your code vulnerable. Extract them. **ALWAYS**.
+
+Repeat with me: **A-L-W-A-Y-S**.
+
+```php
+//Right, perfect, wonderful :)
+$fruit_name = $bdb->selec("name")->from("fruits")->where("id_fruit =", $id_fruit)->exec_one_field();
+
+//WRONG!! NEVER!! SIN!! APOCALYPSE!! THE END OF THE WORLD!!
+$fruit_name = $bdb->selec("name")->from("fruits")->where("id_fruit = $id_fruit")->exec_one_field();
+```
+
+### Learn more about select with one example
+
+```php
+$bdb->select("color", "sum(price)/count(*) as average_price")->from("fruits f", "prices p")->where("f.id_fruit = p.id_fruit")->group_by("color")->having("average_price < 1.5")->exec();
+```
+
+also with only one string parameter for select, for from... etc
+
+```php
+$bdb->select("color, sum(price)/count(*) as average_price")->from("fruits f, prices p")->where("f.id_fruit = p.id_fruit")->group_by("color")->having("average_price < 1.5")->exec();
+```
+
+Both are the same. I love it.
+
+Remember in PHP methods names are case-insensitive, like column names in MySQL:
+
+```php
+$bdb->SELECT("COLOR, SUM(PRICE)/count(*)")->FRom("FRUITS F, prices p")->wHeRe("F.id_fruit = p.ID_FRUIT")->group_BY("color")->exec();
+```
+
+Write as you want.
+
+## Update
+
+Example without external data:
+
+```php
+$bdb->update("fruits")->set("active = 0")->where("id_fruit = 5")->exec();
+```
+As always, don't forget to add quotes when you are setting a string literally. Just like MySQL.
+
+```php
+$bdb->update("fruits")->set(" name = 'coconut', active = 0 ")->where("id_fruit = 5")->exec();
+```
+
+or you can use more than one parameter (on this way you don't need to add quotes)
+
+```php
+$bdb->update("fruits")->set("name =", "coconut")->where("id_fruit = 5")->exec();
+```
+
+Example with external data (with variables):
+
+```php
+//Get data
+$name=$_GET["name"];
+$id_fruit=$_GET["id_fruit"];
+//Execute update query
+$bdb->update("fruits")->set("name = ", $name)->where("id_fruit = ", $id_fruit)->exec();
+```
+
+And don't cry anymore. Don't worry about SQL injections :)
+
+**++/!\++ WARNING 2**: Don't forget to add the equal character after the field name when you are updating. **Just like MySQL** is our motto.
+
+**++/!\++ WARNING 1 (AGAIN)**: Remember that variables inside strings are not checked. Use a single parameter for each variable, like in examples.
+
+##### Updating more than one field with variables (PHP 5.4+ Version) [RECOMMENDED]
+
+```php
+$bdb->update("fruits")->set( ["active =", $active], ["name =", $name] )->where("id_fruit = 5")->exec();
+```
+
+##### Updating more than one field with variables (All PHP Versions)
+
+```php
+$bdb->update("fruits")->set( array("active =", $active), array("name =", $name) )->where("id_fruit = 5")->exec();
+```
+
+Yes, PHP < 5.4 syntax is so ugly. Please, update your PHP version ASAP.
+
+## Insert
+
+As you can imagine...
+
+```php
+$bdb->insert_into("fruits")->values(null, "tomato", 4.2, $my_variable)->exec();
+```
+
+or with custom fields...
+
+```php
+//PHP 5.4+ Version
+$bdb->insert_into("fruits")->values( ["name", $name], ["price", 5.3] )->exec();
+//PHP 5.4- Version
+$bdb->insert_into("fruits")->values( array("name", $name), array("price", 5.3) )->exec();
+```
+
+Don't use the equal character in your insertions. Again, just like MySQL.
+
+## Escape from the cage
+
+All values are passed to MySQL as strings, if you need to execute some MySQL functions, please, escape them with ! character and BananaDB will send the text to MySQL in raw mode, without quotes, out of a string.
+
+```php
+$bdb->update("fruits")->set("updated = ", "!NOW()")->exec();
+```
+And you can add some literals in your queries, like distinct:
+
+```php
+$bdb->select("distinct color")->from("fruits");
+```
+
+If you need more control, you can access directly to the driver...
+
+```php
+$mysqli_driver = $bdb->mysqli;
+```
+
+## Prepared statements
+
+As longer PHP doesn't support references from the method calls, we need to use some hack. The hack is our REF function.
+
+You can use the REF function to send the reference from a variable to BananaDB and use prepared statements as follow:
+
+```php
+//data
+$fruits=["tomato", "banana", "pear", "cherry", "watermelon", "orange"];
+$fruit_name=0;
+//make the query with a reference to $fruit_name using the provided REF function
+$bdb->insert_into("fruits")->values(REF($fruit_name));
+
+//insert all fruits without remake the query (less code, more performance)
+foreach($fruits as $fruit_name)
+   $bdb->exec();
+```
+
+You can use the REF function for any field, for any query (insert, update...). Simply change the value and call to the exec method again.
+
+BananaDB does ++**NOT** send the full query to MySQL++. BananaDB creates a ++prepared statement++, making the repetitive execution very fast. The param binding and all tricky things are automatically managed for you.
+
+For more information about prepared statements read: http://php.net/manual/en/pdo.prepared-statements.php
+
+I know what you're thinking. How REF function works if PHP doesn't support references in method calls? Well, call it magic or read the code ;)
+
+## Some more... After the execution
+
+```php
+$bdb->getLastId();       //returns the last id inserted in auto_increment field
+$bdb->getAffectedRows(); //returns the number of affected rows in the last query
+```
+
+## Order By & Limit & Offset
+
+All examples are the same thing:
+
+```php
+$bdb->select("*")->from("fruits")->order_by("color desc", "name asc")->limit(2)->offset(5);
+```
+```php
+$bdb->select("*")->from("fruits")->order_by("color desc", "name asc")->limit("2")->offset("5");
+```
+```php
+$bdb->select("*")->from("fruits")->order_by("color desc, name asc")->limit(2,5);
+```
+```php
+$bdb->select("*")->from("fruits")->order_by("color desc", "name asc")->limit("2,5");
+```
+
+I hope you get the idea.
+
+##For the future
+
+TODO:
+
+1. **Joins** support
+2. **Not In** support
+3. **Sub-queries** support
+4. **Transactions** support
+
+## Bugs, comments, and more...
+
+Write to lanshor@gmail.com
---
+++ banana_db.php
@@ -0,0 +1,394 @@
+<?php
+
+/**
+ * @author LaNsHoR / http://www.lanshor.com / lanshor@gmail.com
+ */
+
+/****************************************************************************
+The MIT License
+
+Copyright © 2010-2015 BananaDB author
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+****************************************************************************/
+
+class BDB
+{
+   //Util Methods
+   public static function valueArrayFromReference($reference_array, $meta=0x0)
+   {
+      $value_array=array();
+      foreach($reference_array as $reference)
+      {
+         $value_array[]=$reference;
+         if($meta)
+         {
+            $field=$meta->fetch_field();
+            $value_array[$field->name]=$reference;
+         }
+      }
+      return $value_array;
+   }
+   public function getLastId()       {return $this->mysqli?$this->mysqli->insert_id:false;}
+   public function getAffectedRows() {return $this->mysqli?$this->mysqli->affected_rows:false;}
+   //Here the magic begins...
+   public $mysqli=0x0;
+   //========================
+   //Reset the current state when the object needs to be ready for a new query
+   private function reset()
+   {
+      $this->values=array();
+      $this->chain=array();
+      $this->values=array();
+      $this->query_type="";
+      $this->query_string="";
+   }
+   //========================
+   public function connect($host, $user, $password, $database)
+   {
+      if(!$this->mysqli)
+      {
+         $this->mysqli=new mysqli($host, $user, $password, $database);
+         if($this->mysqli->connect_errno)
+            throw new Exception("BananaDB - Connection Error [".$this->mysqli->connect_errno."]: ".$this->mysqli->connect_error);
+         $this->mysqli->set_charset("utf8");
+      }
+   }
+   public function __construct($host, $user, $password, $database)
+   {
+      $this->connect($host, $user, $password, $database);
+      $this->reset();
+   }
+   //========================
+   private function lan_select($field_list)
+   {
+      $this->reset();
+      $this->query_type="select";
+      $this->chain[]="select";
+      foreach($field_list as $index => $field)
+         $this->chain[]=($index?", ":" ").$field;
+   }
+   //========================
+   private function lan_from($field_list)
+   {
+      $this->chain[]=" from ";
+      foreach($field_list as $index => $field)
+         $this->chain[]=($index?", ":" ").$field;
+   }
+   //========================
+   private function lan_condition($op, $condition, $value)
+   {
+      if($value===NULL)
+         $this->chain[]=" $op $condition";
+      else
+      {
+         $this->chain[]=" $op $condition ?";
+         $this->values[]=&$value;
+      }
+   }
+   //========================
+   private function lan_where($condition, $value=NULL) {$this->lan_condition("where", $condition, $value);}
+   private function lan_and($condition, $value=NULL)   {$this->lan_condition("and", $condition, $value);}
+   private function lan_or($condition, $value=NULL)    {$this->lan_condition("or", $condition, $value);}
+   //========================
+   private function lan_delete($value)
+   {  	
+      $this->reset();
+      $this->query_type="delete";
+      $this->chain[]="delete from $value";
+   }
+   //========================
+   private function lan_insert($table)
+   {
+      $this->reset();
+      $this->query_type="insert";
+      $this->chain[]="insert into $table";
+   }
+   //========================
+   private function lan_update($table)
+   {
+      $this->reset();
+      $this->query_type="update";
+      $this->chain[]="update $table ";
+   }
+   //========================
+   private function lan_group_by($value) {$this->chain[]=" group by $value";}
+   //========================
+   private function lan_having($condition, $value=NULL) {$this->lan_condition("having", $condition, $value);}
+   //========================
+   private function lan_limit($from, $to=NULL)
+   {
+   	  $all_together=explode(",", $from);
+   	  if(!$to && count($all_together==2))
+   	  {
+   	  	  $from=$all_together[0];
+   	  	  $to=$all_together[1];
+   	  }
+   	  $limit_string=" limit ".((int)$from);
+      if($to)
+	      $limit_string.=",".((int)$to);
+	  $this->chain[]=$limit_string;
+   }
+   //========================
+   private function lan_offset($offset) {$this->chain[]=" offset ".((int)$offset);}
+   //========================
+   private function lan_order_by($field_list)
+   {
+      $this->chain[]=" order by ";
+      foreach($field_list as $index => $value)
+      {
+         if($index>0)
+            $this->chain[]=", ";
+         $this->chain[]=" $value ";
+      }
+   }
+   //========================
+   private function lan_values($values_array)
+   {
+   	  $values_string="";
+   	  //Custom columns support (["column1", "value1"], ["column2", "value2"])
+   	  if(is_array($values_array[0]))
+   	  {
+   	  	 $values_string.="(";
+   	     foreach($values_array as $index => $field_pair)
+   	     {
+   	     	if($index)
+   	     		$values_string.=", ";	
+   	     	$values_string.=$field_pair[0];
+   	     }
+   	     $values_string.=")";
+   	  }
+      $values_string.=" values(";
+      foreach($values_array as $index => $value)
+      {
+      	 if(is_array($value)) //Custom columns ["column", "value"]
+      	 	$value=$value[1];
+         if(is_object($value))
+            $this->values[]=&$value->ref_value;
+         else
+         {
+            if($value[0]=="!") //Literal parameter
+            {
+               if($index)
+                  $values_string.=",";
+               $values_string.=substr($value,1); //Removes ! from literal paramater
+               continue;
+            }
+            if($value[0]=="\\")
+               $value=substr($value,1); //Removes \ (escape)
+            $this->values[]=trim($value);
+         }
+         if($index)
+            $values_string.=",";
+         $values_string.="?";
+      }
+      $values_string.=")";
+      $this->chain[]=$values_string;
+   }
+   //========================
+   private function lan_set($sets_array)
+   {
+      $this->chain[]="set ";
+      //Format 1: set("field = literal")
+      if(!is_array($sets_array[0]) && !isset($sets_array[1]))
+      {
+         $this->chain[]=$sets_array[0];
+         return;
+      }
+      //Format 2: set("field = ", x)
+      if(!is_array($sets_array[0]))
+      {
+         //Case 2A: Literal parameter
+         if($sets_array[1][0]=="!")
+         {
+            $this->chain[]=$sets_array[0].substr($sets_array[1],1); //Removes ! from literal paramater
+            return;
+         }
+         //Case 2B: Variable parameter
+         $this->chain[]=$sets_array[0]."?";
+         $this->values[]=$sets_array[1];
+         return;
+      }
+      //Format 3: set( ["field1 =",x], ["field2=",y], ... );
+      foreach($sets_array as $index => $pair)
+      {
+         $field=$pair[0];
+         $value=$pair[1];
+         if($index)
+            $this->chain[]=",";
+         //Case 3A: Literal parameter
+         if($value[0]=="!")
+         {
+            $this->chain[]=$field.substr($value,1); //Removes ! from literal parameter
+            continue;
+         }
+         //Case 3B: Variable parameter
+         $this->chain[]=$field."?";
+         $this->values[]=$value;
+      }
+   }
+   //========================
+   private function commonExecutionSteps()
+   {
+      //form the query string
+      $query_string="";
+      foreach($this->chain as $fragment)
+         $query_string.=$fragment;
+      //prepare statement
+      $query=$this->mysqli->prepare($query_string);
+      //bind params
+      $params_string="";
+      $params_list=array(&$params_string);
+      foreach($this->values as &$value)
+      {
+         if(is_string($value))
+            $params_string.="s";
+         elseif(is_integer($value))
+            $params_string.="i";
+         elseif(is_double($value))
+            $params_string.="d";
+         $params_list[]=$value;
+      }
+      //Error control
+      if(!$query)
+         throw new Exception("BananaDB - Parsing Error [".$query_string."]: ".$this->mysqli->error);
+      if(!empty($this->values))
+      {
+         $reflexive_query=new ReflectionClass("mysqli_stmt");
+         $method=$reflexive_query->getMethod("bind_param");
+         $method->invokeArgs($query,$params_list);
+      }
+      //execute
+      $query->execute();
+      return $query;
+   }
+   public function exec()
+   {
+      $query=$this->commonExecutionSteps();
+      if($this->query_type=="select")
+      {
+         //result
+         $return_list=array();
+         $meta=$query->result_metadata();
+         while($field=$meta->fetch_field())
+            $return_list[]=&$row[$field->name];
+         $reflexive_query=new ReflectionClass("mysqli_stmt");
+         $method=$reflexive_query->getMethod("bind_result");
+         $method->invokeArgs($query,$return_list);
+         $total_result=array();
+         while($line=$query->fetch())
+            $total_result[]=BDB::valueArrayFromReference($return_list, $query->result_metadata());
+         if($this->mysqli->error)
+      	 	throw new Exception("BananaDB - MySQL Error [".$this->mysqli->error."]");
+         $query->close();
+         return $total_result;
+      }
+      if($this->query_type=="insert" || $this->query_type=="update" || $this->query_type=="delete")
+      {
+      	 if($this->mysqli->error)
+      	 	throw new Exception("BananaDB - MySQL Error [".$this->mysqli->error."]");
+         $query->close();
+         return true;
+      }
+   }
+   public function exec_one_line()
+   {
+      $result=$this->exec();
+      return $result ? $result[0] : false;
+   }
+   public function exec_one_field()
+   {
+      $result=$this->exec();
+      if(!$result || !$result[0])
+         return false;
+      return $result[0][0];
+   }
+   //========================
+   function __call($function, $args)
+   {
+      $function=strtolower($function);
+      switch($function)
+      {
+         case 'select':
+            $this->lan_select($args);
+            return $this;
+         case 'from':
+            $this->lan_from($args);
+            return $this;
+         case 'where': //where("x >", $x) or where("x = x")
+            if(count($args)>1)
+               $this->lan_where($args[0], $args[1]);
+            else
+               $this->lan_where($args[0]);
+            return $this;
+         case 'and':
+            if(count($args)>1)
+               $this->lan_and($args[0], $args[1]);
+            else
+               $this->lan_and($args[0]);
+            return $this;
+         case 'or':
+            if(count($args)>1)
+               $this->lan_or($args[0], $args[1]);
+            else
+               $this->lan_or($args[0]);
+            return $this;
+         case 'order_by':
+            $this->lan_order_by($args);
+            return $this;
+         case 'insert_into':
+            $this->lan_insert($args[0]);
+            return $this;
+         case 'values':
+            $this->lan_values($args);
+            return $this;
+         case 'update':
+            $this->lan_update($args[0]);
+            return $this;
+         case 'set':
+            $this->lan_set($args);
+            return $this;
+         case 'delete_from':
+            $this->lan_delete($args[0]);
+            return $this;
+         case 'group_by':
+            $this->lan_group_by($args[0]);
+            return $this;
+         case 'having':
+            if(count($args)>1)
+               $this->lan_having($args[0], $args[1]);
+            else
+               $this->lan_having($args[0]);
+            return $this;
+         case 'limit':
+         	if(count($args)>1)
+               $this->lan_limit($args[0], $args[1]);
+            else
+               $this->lan_limit($args[0]);
+            return $this;
+         case 'offset':
+         	$this->lan_offset($args[0]);
+         	return $this;
+         //Unknown operation
+         throw new Exception("BananaDB - Unknown Operation [$function]");
+      }
+   }
+}
+
+class_alias("BDB", "BananaDB");
